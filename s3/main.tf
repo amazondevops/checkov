438204af3396913b63e91c6061e5e008d0200917
SreekanthReddyy <sreekanth.reddyy@jungleegames.com>
2024-04-19 13:22:25 +0530

    initial commit

---
+++ s3/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read" 
+  
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+    Scope = "PCI"
+    product_v2 = "dev"
+    terraform_managed = "true"
+  }
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket = "Test"
+  acl    = "public-read" 
+  
+  tags = {
+    Name        = "Test"
+    Environment = var.environment
+    Scope = "PCI"
+    # product_v2 = "dev"
+    # terraform_managed = "true"
+  }
+}
+
+
+
