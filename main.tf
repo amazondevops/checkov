1067be97beecdb4fb882f5fc272d821d9841466c
SreekanthReddyy <sreekanth.reddyy@jungleegames.com>
2024-05-06 15:27:25 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  for_each = toset(["one", "two", "three"])
+
+  name = "instance-${each.key}"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  count = 3
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
